1d94ad1d331dd6a8985e44ae81630e16b86069f1
Alexey Kostyrin <alexey@Alexeys-MacBook-Air.local>
2018-01-23 18:07:13 +1000

    Initial commit

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"github.com/google/gopacket"
+	"github.com/alex-kostirin/go-netfilter-queue"
+)
+
+func main() {
+	var err error
+
+	nfq, err := netfilter.NewNFQueue(0, 100, netfilter.NF_DEFAULT_PACKET_SIZE)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	defer nfq.Close()
+	packets := nfq.GetPackets()
+
+	for true {
+		select {
+		case p := <-packets:
+			packet := ChangePacket(p.Packet)
+			p.SetVerdictWithPacket(netfilter.NF_ACCEPT, packet)
+		}
+	}
+}
+
+func ChangePacket(p gopacket.Packet) []byte {
+	fmt.Println(p.Dump())
+	return p.Data()
+}
