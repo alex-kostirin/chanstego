d91df0a5f1c737d157b778580de75fd9c1036ad5
alexey <alex.kostirin@ya.ru>
2018-01-24 23:51:42 +1000

    reverted to ip TOS changing

--- main.go
+++ main.go
@@ -4,8 +4,8 @@ import (
 	"fmt"
 	"os"
 	"github.com/google/gopacket"
-	"github.com/alex-kostirin/go-netfilter-queue"
 	"github.com/google/gopacket/layers"
+	"github.com/alex-kostirin/go-netfilter-queue"
 )
 
 func main() {
@@ -22,50 +22,27 @@ func main() {
 	for true {
 		select {
 		case p := <-packets:
-			packet := ChangePacket(p.Packet)
+			packet := ChangePacket(p.Packet, 200)
 			p.SetVerdictWithPacket(netfilter.NF_ACCEPT, packet)
 		}
 	}
 }
 
-func ChangePacket(packet gopacket.Packet) []byte {
-	if ipLayer, tcpLayer := packet.Layer(layers.LayerTypeIPv4), packet.Layer(layers.LayerTypeTCP); ipLayer != nil && tcpLayer != nil {
-		fmt.Println("PACKET IN:")
-		fmt.Println(packet.Data())
+func ChangePacket(packet gopacket.Packet, data uint8) []byte {
+	outgoingPacket := packet.Data()
+	if ipLayer := packet.Layer(layers.LayerTypeIPv4); ipLayer != nil {
 		ipLayer, _ := ipLayer.(*layers.IPv4)
-		tcpLayer, _ := tcpLayer.(*layers.TCP)
-		//echoOption := layers.TCPOption{OptionType: layers.TCPOptionKindEcho, OptionLength: 6, OptionData: []byte{2, 3, 5, 7}}
-		//nopOption := layers.TCPOption{OptionType: layers.TCPOptionKindNop, OptionLength: 1}
-		//hasTcpEchoOption := false
-		//for _, option := range tcpLayer.Options {
-		//	if option.OptionType == layers.TCPOptionKindTimestamps || option.OptionType == layers.TCPOptionKindEcho {
-		//		hasTcpEchoOption = true
-		//		break
-		//	}
-		//}
-		//if !hasTcpEchoOption && tcpLayer.DataOffset <= 13 && len(tcpLayer.Payload) != 0 {
-		//	tcpOptions := make([]layers.TCPOption, len(tcpLayer.Options)+3)
-		//	for i, option := range tcpLayer.Options {
-		//		tcpOptions[i] = option
-		//	}
-		//	tcpOptions[len(tcpLayer.Options)] = echoOption
-		//	tcpOptions[len(tcpLayer.Options)+1] = nopOption
-		//	tcpOptions[len(tcpLayer.Options)+2] = nopOption
-		//	tcpLayer.Options = tcpOptions
-		//	tcpLayer.DataOffset += 2
-		//}
-		tcpLayer.SetNetworkLayerForChecksum(ipLayer)
-		options := gopacket.SerializeOptions{ComputeChecksums: true}
-		buffer := gopacket.NewSerializeBuffer()
-		err := gopacket.SerializeLayers(buffer, options, ipLayer, tcpLayer, gopacket.Payload(tcpLayer.Payload))
-		if err != nil {
-			panic(err)
+		if ipLayer.TOS == 0 {
+			ipLayer.TOS = data
+			fmt.Println(ipLayer.TOS)
+			options := gopacket.SerializeOptions{ComputeChecksums: true}
+			buffer := gopacket.NewSerializeBuffer()
+			err := gopacket.SerializeLayers(buffer, options, ipLayer, gopacket.Payload(ipLayer.Payload))
+			if err != nil {
+				panic(err)
+			}
+			outgoingPacket = buffer.Bytes()
 		}
-		outgoingPacket := buffer.Bytes()
-		fmt.Println("PACKET OUT")
-		fmt.Println(outgoingPacket)
-		return outgoingPacket
-	} else {
-		return packet.Data()
 	}
+	return outgoingPacket
 }
